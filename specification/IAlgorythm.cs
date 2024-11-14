1c26a96d106c28321a5558797f3935f1bd0b1d69
Nikita Batenko <iniktus@mail.ru>
2024-11-14 12:10:35 +0700

    Delete specification directory

--- specification/IAlgorythm.cs
+++
@@ -1,9 +0,0 @@
-using interv_idx = (int idx_begin, int idx_end)
-public interface IAlgorythm {
-    void Process(Curve[] inputCurves, IEnumerable<interv_idx>[] changed_idx_inputCurves, 
-                    Parameter[] inputParameters, (string, bool)[] param_chng_status, Curve[] outputCurves, 
-                        IEnumerable<interv_idx>[] changed_idx_outputCurves);
-    int CountInputCurves{get;}
-    int CountInputParameters{get;}
-    int CountOutputCurves{get;}
-}
